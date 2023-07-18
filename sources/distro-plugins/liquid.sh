e1d67c774ef779ae9ef3307897024ab9f33997a5
thisaducat <131821398+thisaducat@users.noreply.github.com>
2023-07-18 19:18:39 +0300

    Add files via upload

---
+++ sources/distro-plugins/liquid.sh
@@ -0,0 +1,5 @@
+DISTRO_NAME="Liquid"
+DISTRO_COMMENT="rootFS for Liquid"
+
+TARBALL_URL['aarch64']="https://github.com/thisaducat/linux-liquid/releases/download/Liquid/liquid-aarch64.tar.xz"
+TARBALL_SHA256['aarch64']="073aeacac16d256dbd979442c80d63acf3c0bb6f60e3d9813105d7717becf93c"
